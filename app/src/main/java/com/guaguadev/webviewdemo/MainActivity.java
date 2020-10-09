9b8a4a245b23ada8fa34fe06cdcd05fbc1187b26
kongdexi <kongdexi@xgd.com>
2020-10-09 16:20:39 +0800

    feat:项目初始化

---
+++ app/src/main/java/com/guaguadev/webviewdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.guaguadev.webviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.webviewdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
